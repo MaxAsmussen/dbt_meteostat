22685aca34526bca3b9ebd9d5c90be344c03a920
MaxAsmussen <asmussen_max@web.de>
2025-11-28 14:44:12 +0100

    hello

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -2,16 +2,56 @@
     materialized='table'
 ) }}
 
+WITH inbound AS (
+    SELECT dest AS airport, origin
+    FROM {{ ref('prep_flights') }}
+    GROUP BY dest, origin
+),
+outbound AS (
+    SELECT origin AS airport, dest
+    FROM {{ ref('prep_flights') }}
+    GROUP BY origin, dest
+),
+departure_stats AS (
+    SELECT 
+        origin AS airport,
+        COUNT(*) AS total_departures,
+        SUM(cancelled) AS total_cancelled,
+        SUM(diverted) AS total_diverted
+    FROM {{ ref('prep_flights') }}
+    GROUP BY origin
+),
+arrivals_stats AS (
+    SELECT 
+        dest AS airport,
+        COUNT(*) AS total_arrivals,
+        SUM(cancelled) AS total_cancelled_arr,
+        SUM(diverted) AS total_diverted_arr
+    FROM {{ ref('prep_flights') }}
+    GROUP BY dest
+)
 SELECT
     a.faa AS airport,
     a.name,
-    COUNT(DISTINCT CASE WHEN f.dest = a.faa THEN f.origin END) AS unique_inbound_connections,
-    COUNT(DISTINCT CASE WHEN f.origin = a.faa THEN f.dest END) AS unique_outbound_connections,
-    COUNT(*) AS total_planned_flights,
-    SUM(f.cancelled) AS total_cancelled_flights,
-    SUM(f.diverted) AS total_diverted_flights,
-    COUNT(*) - SUM(f.cancelled) - SUM(f.diverted) AS actual_flights
+    COUNT(DISTINCT i.origin) AS unique_inbound_connections,
+    COUNT(DISTINCT o.dest) AS unique_outbound_connections,
+    ds.total_departures + ars.total_arrivals AS total_planned_flights,
+    ds.total_cancelled + ars.total_cancelled_arr AS total_cancelled_flights,
+    ds.total_diverted + ars.total_diverted_arr AS total_diverted_flights,
+    (ds.total_departures + ars.total_arrivals)
+        - (ds.total_cancelled + ars.total_cancelled_arr + ds.total_diverted + ars.total_diverted_arr)
+        AS actual_flights
 FROM {{ ref('prep_airports') }} a
-LEFT JOIN {{ ref('prep_flights') }} f
-ON f.origin = a.faa OR f.dest = a.faa
-GROUP BY a.faa, a.name
+LEFT JOIN inbound i ON i.airport = a.faa
+LEFT JOIN outbound o ON o.airport = a.faa
+LEFT JOIN departure_stats ds ON ds.airport = a.faa
+LEFT JOIN arrivals_stats ars ON ars.airport = a.faa
+GROUP BY 
+    a.faa, 
+    a.name,
+    ds.total_departures, 
+    ds.total_cancelled,
+    ds.total_diverted,
+    ars.total_arrivals,
+    ars.total_cancelled_arr,
+    ars.total_diverted_arr;
